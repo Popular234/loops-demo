24cbda3ad888f1ea2c3b29a3e65b12228954de92
Popular234 <cainjose24@g.crookcountyschools.org>
2022-10-31 13:36:45 -0700

    hey

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("I am alive")
+
+document.write("<h1> Loops Demo</h1>")
+
+const classroom = ["Cain", "Josh", "Jr.", "Kierra", "Sammy", "Mathew"]
+
+let i = 0
+// while(classroom[i]){
+//     console.log(classroom[i]);
+//     i++;
+// }
+
+// while(i<= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// for(i=0; i<=10; i=i+1){
+//     console.log(i)
+// }
+
+// for(i=0; i<=10; i=i+2){
+//     console.log(i)
+// }
+
+// for(i=0; i<=10; i=i+1){
+//     console.log(i*2)
+// }
+
+// for(i=0; i<=10; i=i+1){
+//     console.log(i*10)
+// }
+
+const pets = ["dog ", "cat ", "fish ", "ferret ", "aligator "];
+
+document.write("<h1> our pet store has the following</h1>")
+
+for(i=0; i<pets.length; i++){
+    document.write(pets[i]);
+}
